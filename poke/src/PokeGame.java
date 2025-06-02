f5f16ce2c323e3033ecb6dc809a36f9ce9333e8a
Steven Zhou <z5285528@ad.unsw.edu.au>
2025-06-02 13:18:57 +1000

    PokeGame done, passed the provided test

--- poke/src/PokeGame.java
+++ poke/src/PokeGame.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PokeGame {
 
@@ -11,9 +13,29 @@ public class PokeGame {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                // Parse, create hands, compare, update scores
-                p1_wins += 1;
-                p2_wins += 2;
+                String[] cards = line.trim().split(" ");
+                if (cards.length != 10) {
+                    continue;
+                }
+                
+                List<Card> p1cards = new ArrayList<>();
+                List<Card> p2cards = new ArrayList<>();
+                
+                for (int i = 0; i < 5; i++) {
+                    p1cards.add(new Card(cards[i]));
+                }
+                for (int i = 5; i < 10; i++) {
+                    p2cards.add(new Card(cards[i]));
+                }
+                
+                Hand p1hand = new Hand(p1cards);
+                Hand p2hand = new Hand(p2cards);
+                
+                if (p1hand.compareTo(p2hand) > 0) {
+                    p1_wins++;
+                } else if (p1hand.compareTo(p2hand) < 0) {
+                    p2_wins++;
+                }
             }
         }
         printResults();
